266b153d8118e2fcc94be9a6c6e88b388056f28e
Prath Udhnawala <askforvarn@gmail.com>
2025-06-23 12:39:58 +0530

    Refactor cube function declaration and improve comments for clarity

--- Phase_1/program1.cpp
+++ Phase_1/program1.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int cube(int num)
-{
-    return num * num * num;
-}
+/*
+    1. Meena face an isuue to perform a mathematical operation to find a cube of any number.
+        Write a C++ Program which helps Meena to solve her issue.
+*/
 
-int main() {
+int cube(int num);
+
+int main()
+{
     int num;
     cout << "Enter a number: ";
     cin >> num;
     cout << "The cube of " << num << " is " << cube(num) << endl;
     return 0;
+}
+int cube(int num)
+{
+    return num * num * num;
 }
