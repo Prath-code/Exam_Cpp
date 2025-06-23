466131f0ed5914183bd5b80b9d9c6cf91e306ef9
Prath Udhnawala <askforvarn@gmail.com>
2025-06-23 10:51:50 +0530

    Add C++ program to calculate the cube of a number

---
+++ Phase_1/program1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int cube(int num)
+{
+    return num * num * num;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "The cube of " << num << " is " << cube(num) << endl;
+    return 0;
+}
