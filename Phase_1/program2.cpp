69817b26557261dcfc67922bf432e46f43081576
Prath Udhnawala <askforvarn@gmail.com>
2025-06-23 12:40:07 +0530

    Add C++ program to multiply three numbers

---
+++ Phase_1/program2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*
+    2. Sameer is too weak to find multiplication of any three numbers.
+        Write a C++ Program which helps Sameer to solve his issue.
+*/
+
+int multiply(int a, int b, int c);
+
+int main()
+{
+    int num1, num2, num3;
+    cout << "Enter value num1: ";
+    cin >> num1;
+    cout << "Enter value num2: ";
+    cin >> num2;
+    cout << "Enter value num3: ";
+    cin >> num3;
+    cout << "The multiplication of " << num1 << ", " << num2 << ", and " << num3 << " is " << multiply(num1, num2, num3) << endl;
+    return 0;
+}
+
+int multiply(int a, int b, int c)
+{
+    return a * b * c;
+}
