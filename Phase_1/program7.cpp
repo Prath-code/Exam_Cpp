c83081d94a27c6e628ee25bb656f247ac45d1090
Prath Udhnawala <askforvarn@gmail.com>
2025-06-25 06:46:43 +0530

    Add C++ program to calculate the average of three user-input values

---
+++ Phase_1/program7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout << "Enter value of 'a': ";
+    cin >> a;
+    cout << "Enter value of 'b': ";
+    cin >> b;
+    cout << "Enter value of 'c': ";
+    cin >> c;
+    cout << (a + b + c) / 3;
+    return 0;
+}
