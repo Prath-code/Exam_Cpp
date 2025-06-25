8ac931da0c403d386c8372b01811e6fffc454073
Prath Udhnawala <askforvarn@gmail.com>
2025-06-25 06:47:39 +0530

    Add C++ program to calculate simple interest

---
+++ Phase_1/program12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float p, r, t;
+    cout << "Enter value of 'p': ";
+    cin >> p;
+    cout << "Enter value of 'r': ";
+    cin >> r;
+    cout << "Enter value of 't': ";
+    cin >> t;
+    cout << (p * r * t) / 100;
+    return 0;
+}
